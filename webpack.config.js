692feaddbe810ef7f215fee23a1bef588e10b7e2
kiscon <kiscon@163.com>
2019-05-20 00:25:00 +0800

    添加watch配置

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
 	output: {
 		// path.resolve：解析当前相对路径的绝对路径
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'my-first-webpack.bundle.js'
+		filename: 'bundle.js'
 	},
 	mode: 'development', // 开发模式配置，默认production
+	watch: true, // 开启监视模式，此时webpack指令进行打包会监视文件变化自动打包
 }
