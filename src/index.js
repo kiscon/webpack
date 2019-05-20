f2b5dc579b2c4c98d961f83ae87b004f4f619e4e
kiscon <kiscon@163.com>
2019-05-21 00:22:09 +0800

    webpack-dev-server使用

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 console.log(123)
 console.log(456)
+console.log(789)
+console.log('aaa')
--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,11 @@ module.exports = {
 	},
 	mode: 'development', // 开发模式配置，默认production
 	watch: true, // 开启监视模式，此时webpack指令进行打包会监视文件变化自动打包
+	devServer: {
+		open: true, // 自动开启
+		hot: true,
+		port: 3000,
+		compress: true, //开启压缩
+		contentBase: './src'
+	}
 }
