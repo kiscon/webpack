4e79ce6d3a02c3f49a2f9e7c7cdd283bfe196ae6
kiscon <kiscon@163.com>
2021-07-25 18:02:51 +0800

    update

--- build/build.js
+++ build/build.js
@@ -1,17 +1,41 @@
-const path = require('path')
 const webpack = require('webpack')
-const webpackConfig = require('./webpack.prod')
+const path = require('path')
+const { existsSync } = require('fs')
+const { exec } = require('child_process')
 
-console.log('\x1B[35m%s\x1b[0m', 'building for production...')
+if (!existsSync('dll')) {
+  exec('webpack --config build/webpack.dll.js', {}, function (err, stdout, stderr) {
+    if (err) {
+      return console.error(err)
+    }
+    if (stdout) {
+      console.log('\x1B[32m%s\x1b[0m', stdout.toString())
+      build()
+    }
+    if (stderr) {
+      console.log(data.toString())
+    }
+  })
+} else {
+  build()
+}
 
-webpack(webpackConfig, function (err, stats) {
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n')
-})
+// 构建
+function build() {
+  const webpackConfig = require('./webpack.prod')
+  console.log('\x1B[35m%s\x1b[0m', 'building for production...')
+
+  webpack(webpackConfig, function (err, stats) {
+    if (err) throw err
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n')
+  })
+}
+
+
 
